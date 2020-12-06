1275d5554c35b7043a689598586dcd634116161b
James Sizeland <jamessizeland@gmail.com>
2020-12-06 23:30:55 +0000

    adds progress bar to show CPU usage and change colour when CPU above a given threshold

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -2,6 +2,8 @@
 const path = require("path");
 const { cpu, mem, os } = require("node-os-utils");
 
+let cpuOverload = 5;
+
 // set model
 document.getElementById("cpu-model").innerText = cpu.model();
 
@@ -21,10 +23,21 @@ mem.info().then((info) => {
 setInterval(() => {
   // CPU usage
   cpu.usage().then((info) => {
-    document.getElementById("cpu-usage").innerText = info + "%";
+    document.getElementById("cpu-usage").innerText =
+      Math.round(info * 100) / 100 + "%";
+
+    document.getElementById("cpu-progress").style.width = info + "%";
+
+    //make progress bar red if overloaded
+    if (info >= cpuOverload) {
+      document.getElementById("cpu-progress").style.background = "red";
+    } else {
+      document.getElementById("cpu-progress").style.background = "#30c88b";
+    }
   });
   cpu.free().then((info) => {
-    document.getElementById("cpu-free").innerText = info + "%";
+    document.getElementById("cpu-free").innerText =
+      Math.round(info * 100) / 100 + "%";
   });
   document.getElementById("sys-uptime").innerText = formatTime(+os.uptime());
 }, 2000);
