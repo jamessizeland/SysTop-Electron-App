980165b225bcc5af4c4eb8c8b11a3f33e237159e
James Sizeland <jamessizeland@gmail.com>
2020-12-06 23:02:58 +0000

    adds system info calls to monitor script and displays them on the UI.

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -7,3 +7,12 @@ document.getElementById("cpu-model").innerText = cpu.model();
 
 // Computer name
 document.getElementById("cpu-name").innerText = os.hostname();
+
+// OS
+document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
+
+// Total memory
+mem.info().then((info) => {
+  document.getElementById("mem-total").innerText = info.totalMemMb;
+  console.log(info);
+});
