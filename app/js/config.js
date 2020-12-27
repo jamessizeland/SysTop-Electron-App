8a0f175020c058dccff134f75a0e32463525a5bf
James Sizeland <jamessizeland@gmail.com>
2020-12-27 11:16:32 +0000

    adds submit event to config.js to capture user submitting new settings

--- app/js/config.js
+++ app/js/config.js
@@ -1,7 +1,23 @@
 // import dependencies
 const { ipcRenderer } = require("electron");
+const settingsForm = document.getElementById("settings-form");
 
+// get settings from GUI
 ipcRenderer.on("settings:get", (e, settings) => {
   document.getElementById("cpu-overload").value = settings.cpuOverload;
   document.getElementById("alert-frequency").value = settings.alertFrequency;
+});
+
+// submit settings
+settingsForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const cpuOverload = document.getElementById("cpu-overload").value;
+  const alertFrequency = document.getElementById("alert-frequency").value;
+
+  // send new settings to main process
+  ipcRenderer.send("settings:set", {
+    cpuOverload,
+    alertFrequency,
+  });
+  console.log("settings saved");
 });
